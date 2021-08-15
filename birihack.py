dbbcb7478ba0ca6dac762698c1b5b720bc35a513
BiRi_B@B@ <77091511+Mahfuz-THBD@users.noreply.github.com>
2021-08-15 17:20:29 +0600

    Update birihack.py

--- birihack.py
+++ birihack.py
@@ -21,7 +21,7 @@ print('''
 \033[1;92m Recoded By THBD
 BiRi_B@B@
 Telegram:https://t.me/termux_hacker_bd
-Online Command:\nsudo python Wifi_Hack/birihack.py -i wlan0 -K\n
+One line Command:\nsudo python Wifi_Hack/birihack.py -i wlan0 -K\n
 For Help Type:\npython Wifi_Hack/birihack.py --help\033[0m
 ''')
 class NetworkAddress:
